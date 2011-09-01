15a5b7b5792926168498b41f56bf5588831fda53
James McGrath <james@dealush.com>
2011-09-01 20:24:03 +1000

    removed some unncessary code as Helpers were in fact available without it.

--- lib/shortener/engine.rb
+++ lib/shortener/engine.rb
@@ -5,12 +5,6 @@ module Shortener
   
   class ShortenerEngine < Rails::Engine
  
-    # include the shortener helper methods in the base helper so that
-    # they can be accessed everywhere
-   # initializer 'shortener.helper' do |app|  
-   #   ActionView::Base.send :include, ShortenerHelper
-   # end
-    
   end
   
 end
