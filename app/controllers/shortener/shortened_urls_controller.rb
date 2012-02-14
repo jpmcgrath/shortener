05facaa775d43a4477894b6a2b53706684afd2ef
Michael Reinsch <michael@mobalean.com>
2012-02-14 16:35:57 +0900

    close the DB connection in the thread to avoid deprecation warning

--- app/controllers/shortener/shortened_urls_controller.rb
+++ app/controllers/shortener/shortened_urls_controller.rb
@@ -12,6 +12,7 @@ class Shortener::ShortenedUrlsController < ::ApplicationController
       # browser type, ip address etc.
       Thread.new do
         sl.increment!(:use_count)
+        ActiveRecord::Base.connection.close
       end
       # do a 301 redirect to the destination url
       redirect_to sl.url, :status => :moved_permanently
