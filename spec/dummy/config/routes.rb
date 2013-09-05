7ea75e368d6b64c6d328609fd96659c943b37f87
Francisco Miguel Biete Bañon <fbiete@gmail.com>
2013-09-05 14:44:59 +0200

    Limit match method to :get

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,4 +1,4 @@
 Dummy::Application.routes.draw do
-  match '/:id' => "shortener/shortened_urls#show", via: :all
+  match '/:id' => "shortener/shortened_urls#show", via: :get
   root :to => "application_controller#show"
 end
