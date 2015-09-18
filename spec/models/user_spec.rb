c9e6120e45c3d18767bbc000ec854fc4f5299cb1
James P. McGrath <james@reinteractive.net>
2015-09-18 23:16:31 +1000

    Update for new syntax

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,9 +7,9 @@ describe User do
 
   context 'shortened url created with owner' do
     let (:user) { User.create }
-    let (:url) { Shortener::ShortenedUrl.generate("dealush.com", user) }
+    let (:shortened_url) { Shortener::ShortenedUrl.generate(Faker::Internet.url, owner: user) }
     specify 'shorted_urls will contains the url' do
-      expect(user.shortened_urls).to include url
+      expect(user.shortened_urls).to include shortened_url
       expect(user.shortened_urls.size).to be 1
     end
   end
