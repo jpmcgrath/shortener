6e249051f20e5cf80ce6e95223c4f0caf42a2ac3
James P. McGrath <james@reinteractive.net>
2015-09-20 12:39:05 +1000

    short_url to accept and pass options on to the url_for helper method

--- app/helpers/shortener/shortener_helper.rb
+++ app/helpers/shortener/shortener_helper.rb
@@ -1,9 +1,15 @@
 module Shortener::ShortenerHelper
 
   # generate a url from a url string
-  def short_url(url, owner: nil, custom_key: nil)
-    short_url = Shortener::ShortenedUrl.generate(url, owner: owner, custom_key: custom_key)
-    short_url ? url_for(controller: :"shortener/shortened_urls", action: :show, id: short_url.unique_key, only_path: false) : url
+  def short_url(url, owner: nil, custom_key: nil, expires_at: nil, url_options: {})
+    short_url = Shortener::ShortenedUrl.generate(url, owner: owner, custom_key: custom_key, expires_at: expires_at)
+
+    if short_url
+      options = { controller: :"shortener/shortened_urls", action: :show, id: short_url.unique_key, only_path: false }.merge(url_options)
+      url_for(options)
+    else
+      url
+    end
   end
 
 end
