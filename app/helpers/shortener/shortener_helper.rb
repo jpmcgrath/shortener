70632cd861cdd649fb093da382247fcb8f236753
Steve Hull <p.witty@gmail.com>
2015-11-16 14:32:20 -0800

    Support passing a ShortenedUrl to the short_url helper
    also adds specs for changes to the helper

--- app/helpers/shortener/shortener_helper.rb
+++ app/helpers/shortener/shortener_helper.rb
@@ -2,7 +2,12 @@ module Shortener::ShortenerHelper
 
   # generate a url from a url string
   def short_url(url, owner=nil)
-    short_url = Shortener::ShortenedUrl.generate(url, owner)
+    short_url = if url.is_a? Shortener::ShortenedUrl
+                  url
+                else
+                  Shortener::ShortenedUrl.generate(url, owner)
+                end
+
     if short_url
       if host = Shortener.default_host.presence
         url_for(host: host, controller: :"shortener/shortened_urls", action: :show, id: short_url.unique_key, only_path: false)
--- spec/helpers/shortener_helper_spec.rb
+++ spec/helpers/shortener_helper_spec.rb
@@ -23,5 +23,27 @@ describe Shortener::ShortenerHelper, type: :helper do
         expect(helper.short_url(destination)).to eq destination
       end
     end
+
+    context 'a short url passed in' do
+      let(:destination) { shortened_url }
+      let(:shortened_url) { instance_double('ShortenedUrl', unique_key: '12345') }
+
+      it "renders the url" do
+        expect(helper.short_url(destination)).to eq "http://test.host/12345"
+      end
+    end
+
+    context 'a default host set' do
+      let(:shortened_url) { instance_double('ShortenedUrl', unique_key: '12345') }
+
+      after {
+        Shortener.default_host = nil
+      }
+
+      it "shortens the url" do
+        Shortener.default_host = "http://foo.co"
+        expect(helper.short_url(destination)).to eq "http://foo.co/12345"
+      end
+    end
   end
 end
