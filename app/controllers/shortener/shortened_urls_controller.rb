cf973466a0024de0567accb1c04694458dc6067a
Sacha <sacha@hellojam.fr>
2016-01-15 18:58:53 +0100

    Exclude crawler from url usage count

--- app/controllers/shortener/shortened_urls_controller.rb
+++ app/controllers/shortener/shortened_urls_controller.rb
@@ -1,9 +1,15 @@
 class Shortener::ShortenedUrlsController < ActionController::Base
+  include Shortener
 
   def show
     token = ::Shortener::ShortenedUrl.extract_token(params[:id])
-    url   = ::Shortener::ShortenedUrl.fetch_with_token(token: token, additional_params: params)
+    url   = ::Shortener::ShortenedUrl.fetch_with_token(token: token, additional_params: params, track: is_not_crawler?)
     redirect_to url[:url], status: :moved_permanently
   end
 
+  private
+
+  def is_not_crawler?
+    CRAWLERS_USER_AGENT_LIST.exclude? request.user_agent
+  end
 end
