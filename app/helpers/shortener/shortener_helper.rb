a94a3f2261352ecf1eb3d66ae2e79701f1d991b2
j <dev@jamespmcgrath.com>
2016-11-04 21:52:19 +1000

    Category param for ShortenerHelper, bump version

--- app/helpers/shortener/shortener_helper.rb
+++ app/helpers/shortener/shortener_helper.rb
@@ -1,13 +1,15 @@
 module Shortener::ShortenerHelper
 
   # generate a url from a url string
-  def short_url(url, owner: nil, custom_key: nil, expires_at: nil, fresh: false, url_options: {})
-    short_url = Shortener::ShortenedUrl.generate(url,
-                                                  owner:      owner,
-                                                  custom_key: custom_key,
-                                                  expires_at: expires_at,
-                                                  fresh:      fresh
-                                                )
+  def short_url(url, owner: nil, custom_key: nil, expires_at: nil, fresh: false, category: nil, url_options: {})
+    short_url = Shortener::ShortenedUrl.generate(
+      url,
+      owner:      owner,
+      custom_key: custom_key,
+      expires_at: expires_at,
+      fresh:      fresh,
+      category:   category
+    )
 
     if short_url
       options = { controller: :"/shortener/shortened_urls", action: :show, id: short_url.unique_key, only_path: false }.merge(url_options)
